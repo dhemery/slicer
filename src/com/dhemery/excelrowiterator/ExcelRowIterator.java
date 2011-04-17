b1792951737ba7078cbe0813f07b2bb2d7422bff
Dale Emery <dale@dhemery.com>
2011-04-17 00:38:14 -0700

    Convert sequence of if statements to lookup+dispatch

--- src/com/dhemery/excelrowiterator/ExcelRowIterator.java
+++ src/com/dhemery/excelrowiterator/ExcelRowIterator.java
@@ -4,39 +4,76 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.lang.reflect.Method;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.apache.poi.ss.usermodel.Cell;
-import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
 
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
 public class ExcelRowIterator implements Iterator<Object[]> {
+	public interface CellValueConverter {
+		Object valueOf(Cell cell);
+	}
 	private int rowNumber;
 	private Sheet sheet;
 	private int numberOfRows;
 	private final Class<?>[] parameterTypes;
+	private final Map<Class<?>,CellValueConverter> convertersByType = new HashMap<Class<?>,CellValueConverter>();
 
 	public ExcelRowIterator(String excelFileName, Method method) throws FileNotFoundException, IOException {
 		sheet = getSheet(excelFileName);
 		numberOfRows = numberOfRowsIn(sheet);
 		parameterTypes = method.getParameterTypes();
 		rowNumber = 0;
+		createConverters();
+	}
+
+	private void createConverters() {
+		CellValueConverter booleanConverter = new CellValueConverter() {
+			@Override
+			public Object valueOf(Cell cell) {
+				return cell.getBooleanCellValue();
+			}		
+		};
+
+		CellValueConverter doubleConverter = new CellValueConverter() {
+			@Override
+			public Object valueOf(Cell cell) {
+				return cell.getNumericCellValue();
+			}		
+		};
+
+		CellValueConverter integerConverter = new CellValueConverter() {
+			@Override
+			public Object valueOf(Cell cell) {
+				return (int) cell.getNumericCellValue();
+			}		
+		};
+
+		CellValueConverter stringConverter = new CellValueConverter() {
+			@Override
+			public Object valueOf(Cell cell) {
+				return cell.getStringCellValue();
+			}		
+		};
+		convertersByType.put(boolean.class, booleanConverter);
+		convertersByType.put(Boolean.class, booleanConverter);
+		convertersByType.put(double.class, doubleConverter);
+		convertersByType.put(Double.class, doubleConverter);
+		convertersByType.put(int.class, integerConverter);
+		convertersByType.put(Integer.class, integerConverter);
+		convertersByType.put(String.class, stringConverter);
 	}
 
 	private Sheet getSheet(String excelFileName) throws FileNotFoundException, IOException {
 		FileInputStream fileInputStream = new FileInputStream(excelFileName);
 		HSSFWorkbook workbook = new HSSFWorkbook(fileInputStream);
 		return workbook.getSheetAt(0);
-	}
-
-	private int numberOfRowsIn(Sheet sheet) {
-		int lastRowNum = sheet.getLastRowNum();
-		if(lastRowNum > 0) return lastRowNum + 1;
-		return sheet.getPhysicalNumberOfRows();
 	}
 
 	@Override
@@ -48,42 +85,26 @@ public class ExcelRowIterator implements Iterator<Object[]> {
 	public Object[] next() {
 		List<Object> parameterValues = new ArrayList<Object>();
 		for(int columnNumber = 0 ; columnNumber < parameterTypes.length ; columnNumber++) {
-			parameterValues.add(getValue(rowNumber, columnNumber));
+			parameterValues.add(valueOfCell(columnNumber));
 		}
 		rowNumber++;
 		return parameterValues.toArray(new Object[0]);
 	}
 
-	private Object getValue(int rowNumber, int columnNumber) {
-		Row row = sheet.getRow(rowNumber);
-		Cell cell = row.getCell(columnNumber);
-		Class<?> parameterType = parameterTypes[columnNumber];
-		if(parameterType.isAssignableFrom(boolean.class)) {
-			return cell.getBooleanCellValue();
-		}
-		if(parameterType.isAssignableFrom(Boolean.class)) {
-			return cell.getBooleanCellValue();
-		}
-		if(parameterType.isAssignableFrom(double.class)) {
-			return cell.getNumericCellValue();
-		}
-		if(parameterType.isAssignableFrom(Double.class)) {
-			return cell.getNumericCellValue();
-		}
-		if(parameterType.isAssignableFrom(int.class)) {
-			return (int) cell.getNumericCellValue();
-		}
-		if(parameterType.isAssignableFrom(Integer.class)) {
-			return (int) cell.getNumericCellValue();
-		}
-		if(parameterType.isAssignableFrom(String.class)) {
-			return cell.getStringCellValue();
-		}
-		return null;
+	private int numberOfRowsIn(Sheet sheet) {
+		int lastRowNum = sheet.getLastRowNum();
+		if(lastRowNum > 0) return lastRowNum + 1;
+		return sheet.getPhysicalNumberOfRows();
 	}
 
 	@Override
 	public void remove() {
 		throw new NotImplementedException();
 	}
-}
+	
+	private Object valueOfCell(int columnNumber) {
+		Cell cell = sheet.getRow(rowNumber).getCell(columnNumber);
+		Class<?> type = parameterTypes[columnNumber];
+		return convertersByType.get(type).valueOf(cell);
+	}
+	}
